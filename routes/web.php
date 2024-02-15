3e8bbb9f48e3239648877a4ac08e3109939cbfb9
Sergio Córdoba <scordobamuriel@gmail.com>
2024-02-15 16:17:10 -0500

    Product created fixed

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,5 @@ Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name("hom
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
-Route::get('/products/created', 'App\Http\Controllers\ProductController@created')->name("product.created");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
 
